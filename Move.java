58456def3436c1a0f8f7291a0b9ebd967a380c18
Evan M Doyle <cs12skd@acs-cseb240-15.ucsd.edu>
2015-04-25 21:08:08 -0700

    Added setters/getters and implemented a 4-arg constructor

--- Move.java
+++ Move.java
@@ -5,9 +5,46 @@ public class Move {
 	private Piece attacker, victim;
 	private Space beginSpace, endSpace;
 	
-	public Move(){
+	public Move(Piece attacker, Piece victim, Space beginSpace,
+			Space endSpace){
+
+		this.attacker = attacker;
+		this.victim = victim;
+		this.beginSpace = beginSpace;
+		this.endSpace = endSpace;
 		
-		
+	}
+
+	public Piece getAttacker(){
+		return attacker;
+	}
+
+	public Piece getVictim(){
+		return victim;
+	}
+
+	public void setAttacker(Piece att){
+		attacker = att;
+	}
+	
+	public void setVictim(Piece vic){
+		victim = vic;
+	}
+
+	public void setBegin(Space beg){
+		beginSpace = beg;
+	}
+
+	public void setEnd(Space end){
+		endSpace = end;
+	}
+
+	public Space getBegin(){
+		return beginSpace;
+	}
+
+	public Space getEnd(){
+		return endSpace;
 	}
 
 }
