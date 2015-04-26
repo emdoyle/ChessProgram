c75ea68ff98bf0fefccb380ec57c98bf263ab340
Evan M Doyle <cs12skd@acs-cseb240-15.ucsd.edu>
2015-04-25 20:23:25 -0700

    Started adding setup and input

--- GameController.java
+++ GameController.java
@@ -1,4 +1,13 @@
 
 public class GameController {
 
+	Board currentBoard;
+
+	public static void main(String[] args){
+		System.out.println("Chess program has started, about to display board\n");
+		currentBoard = new Board(8, 8);
+		currentBoard.displayBoard();
+		System.out.print("Please enter a move:\n[1.(move), 1 (move), or (move)]\n");
+	}
+
 }
