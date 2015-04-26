aa5a3850665fdd0a0107942a240d6f1f306d4af0
Evan M Doyle <cs12skd@acs-cseb240-15.ucsd.edu>
2015-04-25 19:21:17 -0700

    Added constructor and canReach to Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,4 +1,21 @@
 
 public class Bishop extends Piece{
 
+	public Bishop(){
+		symbol = 'B';
+	}
+
+	public boolean canReach(Space dest){
+		currentFile = currentSpace.getFile();
+		currentRank = currentSpace.getRank();
+		destFile = dest.getFile();
+		destRank = dest.getRank();
+
+		currIndex = currentRank*8 + currentFile;
+		destIndex = destRank*8 + destFile;
+
+		return (currIndex - destIndex) % 7 == 0 ||
+							(currIndex = destIndex) % 9 == 0;
+	}
+
 }
