1ecac6ef0851032639bbb3527c3e932d5a00bd0a
Evan M Doyle <cs12skd@acs-cseb240-15.ucsd.edu>
2015-04-25 20:03:17 -0700

    Removed private tags from instance vars in Piece

--- Piece.java
+++ Piece.java
@@ -1,14 +1,24 @@
 
 public abstract class Piece {
 	
-	private Space currentSpace;
-	private char symbol;
-	private char team;
+	Space currentSpace;
+	char symbol;
+	char team;
 	
 	public abstract boolean canReach(Space dest);
 
 	public Space getSpace(){
 		return currentSpace;
+	}
+
+	public char getTeam(){
+		return team;
+	}
+	public char getSymbol(){
+		return symbol;
+	}
+	public void setSymbol(char sym){
+		symbol = sym;
 	}
 	public void setSpace(Space dest){
 		currentSpace = dest;
