d62f1a5509afe7b034b1ad5c18595de9c7311b93
Jon Topielski <jtopiels@ucsd.edu>
2015-04-25 19:31:55 -0700

    Updated King.java

--- King.java
+++ King.java
@@ -1,4 +1,21 @@
+// King chess piece & subclass of Piece.java
+public class King extends Piece {
 
-public class King extends Piece{
+  // constructor
+  public King( Space space ) {
+    this.currentSpace = space;
+    this.symbol = 'K';
+  }
 
+  public boolean canReach( Space dest ) {
+    if (Math.abs( currentSpace.rank - dest.rank ) > 1) {
+      return false;
+    }
+
+    if (Math.abs( currentSpace.file - dest.file ) > 1) {
+      return false;
+    }
+
+    return true;
+  }
 }
