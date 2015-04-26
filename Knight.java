1384c1a9f35f474b5b23a763fe4e3c5123479707
Evan M Doyle <cs12skd@acs-cseb240-15.ucsd.edu>
2015-04-25 19:28:17 -0700

    Added constructor and canReach to Knight.java

--- Knight.java
+++ Knight.java
@@ -1,4 +1,23 @@
 
 public class Knight extends Piece{
 
+	public Knight(){
+		symbol = 'N';
+	}	
+
+	public boolean canReach(Space dest){
+
+		currentFile = currentSpace.getFile();
+		currentRank = currentSpace.getRank();
+		destFile = dest.getFile();
+		destRank = dest.getRank();
+
+		currIndex = currentRank*8 + currentFile;
+		destIndex = destRank*8 + destFile;
+
+		diff = Math.abs(currIndex-destIndex);
+		return diff == 6 || diff == 10 || diff == 15 || diff == 17;
+
+	}
+
 }
