1a8519c270ca72f11bb94d8a96ff8867ddd172c7
Jon Topielski <jtopiels@ucsd.edu>
2015-04-25 21:31:56 -0700

    Updated move interpreter

--- MoveInterpreter.java
+++ MoveInterpreter.java
@@ -12,7 +12,11 @@ public class MoveInterpreter {
 	}
 	
 	public Move interpretMoves(String line){
-		
+		if (line == "" || line.length() == 1) return null; // empty string / bad input
+		if (line.length() > 10) return null; // optimization for now (until accept whole games)
+		char firstChar = line.charAt(0);
+		if ( firstChar >= 'a' && firstChar <= 'h' ) { // we know its pawn move
+			}
 		
 		return null;
 	}
