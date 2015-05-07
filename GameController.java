5f6581f7d5afb4ff96e71057c7b364fe1e0d75f0
Jonathan Topielski <jtopiels@ucsd.edu>
2015-05-06 21:29:33 -0700

    Started working on Move Interpreter and the associated Game Controller

--- GameController.java
+++ GameController.java
@@ -3,11 +3,26 @@ import java.util.Scanner;
 public class GameController {
 
 	public static void main(String[] args){
+		Scanner input = new Scanner( System.in );	
 		System.out.println("Chess program has started, about to display board\n");
 		Board currentBoard = new Board(8, 8);
-		currentBoard.displayBoard();
-		System.out.print("Please enter a move:\n[1.(move), 1 (move), or (move)]\n");
-		Scanner input = new Scanner(System.in);
+		String selectedMove;
+		MoveInterpreter interp = new MoveInterpreter();
+		char currentMove = 'w';
+		/*
+		while ( true ) {
+			currentBoard.displayBoard();
+			System.out.print("Please enter a move:\n[1.(move), 1 (move), or (move)]\n");
+			selectedMove = input.next();
+			if ( !interp.isValidMove( selectedMove ) ) {
+				System.err.println( "You entered an invalid move!");
+				continue;
+			}
+
+			currentMove = ( currentMove == 'w' ) ? 'b' : 'w';
+		}*/
+	
+		/*	
 		Move move1 = new Move(currentBoard.getSpacesArray()[12].getPiece(), null,
 				currentBoard.getSpacesArray()[12], currentBoard.getSpacesArray()[28]);
 		executeMove(currentBoard, move1);
@@ -23,7 +38,7 @@ public class GameController {
 		Move move3 = new Move(currentBoard.getSpacesArray()[28].getPiece(), currentBoard.getSpacesArray()[35].getPiece(), currentBoard.getSpacesArray()[28], currentBoard.getSpacesArray()[35]);
 		executeMove(currentBoard, move3);
 		currentBoard.displayBoard();
-
+*/
 		/*MoveInterpreter interp = new MoveInterpreter();
 		while(input.hasNext()){
 			Move currentMove = interp.interpret(input.next());
--- MoveInterpreter.java
+++ MoveInterpreter.java
@@ -1,24 +1,65 @@
-
 import java.util.Scanner;
-
 public class MoveInterpreter {
-	
+
 	private Scanner input;
 
-	public MoveInterpreter(){
-		
+	public MoveInterpreter(){		
 		input = new Scanner(System.in);
-
 	}
-	
-	public Move interpretMoves(String line){
-		if (line == "" || line.length() == 1) return null; // empty string / bad input
-		if (line.length() > 10) return null; // optimization for now (until accept whole games)
-		char firstChar = line.charAt(0);
-		if ( firstChar >= 'a' && firstChar <= 'h' ) { // we know its pawn move
+
+	public boolean isValidMove( String line ) {
+		char firstChar, secondChar, thirdChar, fourthChar;
+		if ( line == "" || line.length() == 1 ) {
+			return false;
+		}
+
+		if ( line.length() > 10 ) {
+			return false;
+		}
+
+		firstChar = line.charAt(0);
+		secondChar = line.charAt(1);
+
+		if ( firstChar >= 'a' && firstChar <= 'h' ) { // we know its a pawn move
+			if ( secondChar == 'x' ) { // we know its a capture
+				if ( line.length() != 4 ) { // must be size 4
+					return false;
+				}
+				thirdChar = line.charAt(2);
+				fourthChar = line.charAt(3);
+				if ( thirdChar < 'a' || thirdChar > 'h' ) {
+					return false;
+				}
+				if ( fourthChar < '1' || fourthChar > '8' ) {
+					return false;
+				}
 			}
+			if ( secondChar < '1' || secondChar > '8' ) {
+				return false;
+			}
+		}
+		return true;
+	}
 		
+	
+	public Move interpretMove ( String move, Board board, char team ) {
+		char firstChar = move.charAt(0);
+		char secondChar = move.charAt(1);
+		char thirdChar, fourthChar;
+		int columnAdder = (firstChar - 'a') + 1;
+		int rankAdder, currentRank = 0;
+		if ( secondChar != 'x' ) {
+			currentRank = ( secondChar - '0' ) - 1;
+			currentRank *= 8;
+		}
+		int moveSpace = currentRank + columnAdder;
+		Space currentSpace = board.getSpacesArray()[moveSpace];
+		if ( currentSpace.isOccupied() ) {
+			System.out.println( "Cannot move to an occupied space!" );
+			return null;
+		}	
+
 		return null;
 	}
-	
 }
+
